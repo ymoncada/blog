e62eadf48def5a7b75c2b583d286abb10ae499dc
Yoselin Moncada <yoselinmc92@hotmail.com>
2017-09-18 21:19:31 +0200

    Primera Clase

---
+++ app/Core/Entities/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Core\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes;
+    protected $table = 'categories';
+    //protected $connection = 'mysql';
+    protected $fillable = ['name','descrition'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class,'category_id');
+    }
+}
